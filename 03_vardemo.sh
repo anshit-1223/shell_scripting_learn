79b49f4fe09e38d80eb430c5f3f250853246d1ed
Anshit Verma <anshit@kali>
2025-04-12 22:25:09 +0530

    variable demo

---
+++ 03_vardemo.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+#Script to show how to use variables
+
+id=1
+name=AnshitVerma
+age=24
+
+echo "My name is $name and age is $age"
+
+name="Abhishek Gupta"
+
+echo "My Friend name is $name"
+
+#To store output in a variable
+Hostname=$(hostname)
+echo $Hostname
